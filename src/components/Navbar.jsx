177567c5c7eadc7ade68fa211ac1ae481521e035
tumon <tumonkhn@gmail.com>
2022-08-19 14:17:30 +0600

    init

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,28 @@
 import Image from "next/image"
-
-
+import { RiShoppingBasketLine, RiUserLine, RiMenuLine } from "react-icons/ri"
 const Navbar = () => {
     return (
-        <div className=" ring-2 text-amber-500 text-5xl font-extrabold">
-            <span>
-                <Image src="/res-logo.png" height='40' width='40' alt='logo' />
-            </span>
+        <div className=" mx-2 py-1 flex justify-between">
+            <div className=" flex flex-col items-center justify-between">
 
-            <div>
+                <Image src="/res-logo.png" height='40' width='40' alt='logo' />
+                <span className=" font-rockNroll block">
+
+                </span>
             </div>
-        </div>
+
+            <div className=" flex gap-4 items-center text-xl">
+                <span className=" relative">
+                    <RiShoppingBasketLine className=" text-xl" />
+                    <span className=" absolute bg-red-500 
+                    rounded-full h-4 w-4 text-white font-bold
+                     top-[-6px] text-sm flex 
+                    justify-center items-center  right-[-6px]">0</span>
+                </span>
+                <RiUserLine />
+                <RiMenuLine />
+            </div>
+        </div >
     )
 }
 export default Navbar
