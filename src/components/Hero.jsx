7246639121e4c962e7fc4f84816d51ff57f222a0
tumon <tumonkhn@gmail.com>
2022-08-19 16:33:52 +0600

    init

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,25 +2,45 @@ import Image from "next/image"
 
 const Hero = () => {
     return (
-        <div className=" bg-red-300 w-full h-64 flex">
-            <div>
-
-                <p className=" font-rockNroll">Easy way to make an order</p>
-                <p className="text-2xl font-rockNroll">
-                    <span className=" text-red-500 ">
+        <div className=" md:flex w-full md:px-32">
+            <div className=" flex flex-col items-center md:items-start w-full">
+                <p className=" font-rockNroll font-extrabold my-2">Easy way to make an order</p>
+                <p className="text-2xl my-2 font-extrabold font-rockNroll">
+                    <span className=" text-red-500">
                         HUNGRY </span>
                     Just wait</p>
-                <p className="text-2xl text-indigo-900 font-rockNroll">
+                <p className="text-2xl font-bold my-2 text-indigo-900 font-rockNroll">
                     foot at
                     <span className=" text-red-500"> your door </span> </p>
+
+                <p className=" text-xs text-center px-20 md:px-0 my-6 text-gray-500">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Qui magni delectus tenetur autem, sint veritatis!</p>
+
+                <div className=" flex gap-6 my-6">
+                    <button className=" transition hover:bg-gray-700
+                    text-xs bg-red-500 px-3 py-1 rounded text-white">
+                        {` Order now >`}
+                    </button>
+
+                    <button className="text-xs text-red-500 ring-1 ring-red-500 px-3 py-1 rounded">
+                        See all foods
+                    </button>
+                </div>
             </div>
 
-            <div>
-                <Image src='/hero.png' height={100} width={100}
-                    layout="responsive" alt="hero banner"
-                    className=" object-fill"
-                />
+            <div className=" flex flex-col items-center">
+                <div className=" flex h-[240px] w-[240px] 
+            md:min-h-[500px] md:min-w-[500px] relative">
+                    <Image src='/hero.png'
+                        layout="fill" alt="hero banner"
+                        className=" object-fill"
+                    />
+                </div>
             </div>
+
+
+
         </div >
     )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import Hero from "../components/Hero";
 export default function Home() {
   return (
     <div>
-      <section className=" flex justify-center mt-10 ">
+      <section className=" flex justify-centerw-full mt-10 ">
         <Hero />
       </section>
     </div>
