2fe650d29a52501d22e884057375b7018964bffa
tumon <tumonkhn@gmail.com>
2022-10-20 16:32:43 +0600

    init

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import { useEffect } from "react";
 import Desc2 from "../components/Desc2";
 import FoodCategory from "../components/FoodCategory";
@@ -10,11 +11,33 @@ import WhyHungry from "../components/WhyHungry";
 export default function Home() {
   useEffect(() => {
     // on Android Chrome
+
     window.matchMedia("(display-mode: standalone)").matches;
   }, []);
 
+  function openFullscreen() {
+    document.documentElement.requestFullscreen();
+    // if (elem.requestFullscreen) {
+    //   elem.requestFullscreen();
+    // } else if (elem.requestFullscreen) {
+    //   /* Safari */
+    //   elem.requestFullscreen();
+    // } else if (elem.requestFullscreen) {
+    //   /* IE11 */
+    //   elem.requestFullscreen();
+    // }
+  }
+
   return (
     <div>
+      <Head>
+        <title> HungryNaki</title>
+        <meta
+          name="viewport"
+          content="initial-scale=1.0, width=device-width; viewport-fit=cover"
+        />
+      </Head>
+      <h1 onClick={openFullscreen}> Hello</h1>
       <Hero />
       <FoodCategory />
       <Desc2 />
