152f1611fad874ca2d0915e9cba25f616d0c4495
tumon <tumonkhn@gmail.com>
2022-10-22 03:02:56 +0600

    init

--- next.config.js
+++ next.config.js
@@ -2,11 +2,11 @@ const withPWA = require("next-pwa")({
   dest: "public",
   register: true,
   skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
 });
 
 module.exports = withPWA({
   reactStrictMode: true,
   swcMinify: true,
   experimental: { images: { allowFutureImage: true } },
-  disable: process.env.NODE_ENV === "development",
 });
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,17 +5,15 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <meta name="mobile-web-app-capable" content="yes" />
-          <meta name="apple-mobile-web-app-capable" content="yes" />
-          <meta
-            name="apple-mobile-web-app-status-bar-style"
-            content="black-translucent"
-          />
           <link rel="manifest" href="/manifest.json" />
           <link rel="apple-touch-icon" href="/icon.png"></link>
           <meta name="theme-color" content="#fff" />
 
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.googleapis.com"
+            crossOrigin
+          />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
           <link
             href="https://fonts.googleapis.com/css2?family=RocknRoll+One&display=swap"
